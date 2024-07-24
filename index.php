e1d6ff99d67cbb3d5d281d2f9c6c490eb92f12a6
viniciius254 <viniciius254@gmail.com>
2024-07-24 15:17:41 -0300

    Enviando a pagina do jogo, só com parte visual, não tem backend

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Adventure</title>
+    <link rel="stylesheet" href="styles/style.css">
+
+</head>
+<body>
+    <div class="cinza">
+        <h1>Bem-vindo ao TestAdventure</h1>
+
+        <h3>
+            Nesse game você precisara ajudar a dora a chegar na casa do botas!!!
+        </h3>
+       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolorem excepturi cum eligendi hic! Nam, similique. Labore impedit non consequatur dolorum rem nisi sint neque blanditiis quis distinctio. Eius, quae.</p>
+       <div class="text-area">
+        
+       
+
+        <div class="awnser">
+            <input type="text" placeholder="  Escreva aqui!!!"><button type="submit">Enviar</button>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
